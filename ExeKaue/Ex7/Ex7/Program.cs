2198327ee021b1873481e566f13c30dae93fed43
kauelima00 <klimaml022@gmail.com>
2026-03-10 13:52:25 -0300

    Add files via upload

---
+++ ExeKaue/Ex7/Ex7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float dolar;
+            float reais;
+            Console.WriteLine("Digite um valor em dolar");
+            dolar = float.Parse(Console.ReadLine());
+            reais = dolar * 5.14f;
+            Console.WriteLine("{0} reais",reais);
+        }
+    }
+}
